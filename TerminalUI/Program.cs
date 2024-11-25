13a9b207e9fa94f29922a0fa12f3d0c455ce49c4
TheD0ubleC <cc@scmd.cc>
2024-11-26 02:56:56 +0800

    changed

--- TerminalUI/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("If you want to see a demonstration of the effect,Please run TerminalUI.Demo.");
-        Console.ReadKey();
-    }
-}
